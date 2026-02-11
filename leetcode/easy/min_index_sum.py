ca252ac1ce681455b094ab28c81ca1183900a708
mogesTesema <moges2421@gmail.com>
2026-02-11 09:43:45 +0300

    one problem solved

---
+++ leetcode/easy/min_index_sum.py
@@ -0,0 +1,25 @@
+from typing import List
+class Solution:
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+
+
+        list1_set = set(list1)
+        list2_set = set(list2)
+        common = list1_set & list2_set
+        ans = []
+        least_index = float("inf")
+
+        for chars in common:
+            index_one = list1.index(chars)
+            index_two = list2.index(chars)
+            index_sum = index_one + index_two
+
+            if index_sum < least_index:
+                ans = [chars]
+                least_index = index_one + index_two
+            elif index_sum == least_index:
+                ans.append(chars)
+        return ans
+
+
+        
---
+++ leetcode/easy/missing_number.py
@@ -0,0 +1,7 @@
+from typing import List
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+       original_set = set(range(0,len(nums)+1))
+       x = original_set - set(nums)
+       for num in x:
+        return num
