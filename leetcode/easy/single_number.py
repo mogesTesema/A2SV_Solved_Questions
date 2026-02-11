f69450fca97e879a2988e13a8fc13395e9bc5945
mogesTesema <moges2421@gmail.com>
2026-02-11 10:12:40 +0300

    one problem solved

---
+++ leetcode/easy/single_number.py
@@ -0,0 +1,13 @@
+from collections import Counter
+from typing import List
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+
+        freq_nums = Counter(nums)
+
+        for num in freq_nums:
+        
+            if freq_nums[num] == 1:
+                
+                return num
+        
