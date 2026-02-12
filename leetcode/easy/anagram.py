35a5e1b60615ca17f33cb8752aceb96d0d2e44c5
mogesTesema <moges2421@gmail.com>
2026-02-12 13:34:25 +0300

    anagram  leetcode medium one problem solved

---
+++ leetcode/easy/anagram.py
@@ -0,0 +1,17 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        t_count = Counter(t)
+        s_count = Counter(s)
+
+        if len(t_count) != len(s_count):
+            return False
+        for key,val in t_count.items():
+            if key not in s_count:
+                return False
+            if s_count[key] != val:
+                return False
+
+        return True
+        
