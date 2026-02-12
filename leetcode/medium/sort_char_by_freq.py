5e6bd9b4aba83cc7fe46e58c4ada5bcf21308623
mogesTesema <moges2421@gmail.com>
2026-02-12 15:22:02 +0300

    leetcode one problem solved

---
+++ leetcode/medium/sort_char_by_freq.py
@@ -0,0 +1,16 @@
+from collections import Counter
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        str_freq = Counter(s)
+        freq_counted= []
+        for key,val in str_freq.items():
+            freq_counted.append([val,key])
+        
+
+        freq_counted.sort(reverse=True)
+        ans = ""
+
+        for val,key in freq_counted:
+            ans += val*key
+        
+        return ans
