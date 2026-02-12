cfe19434dbc7e68599d03ff209aaf4a22d3be505
mogesTesema <moges2421@gmail.com>
2026-02-12 11:06:57 +0300

    anagram group  leetcode medium one problem solved

---
+++ leetcode/medium/group_anagram.py
@@ -0,0 +1,31 @@
+from typing import List
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+
+        anagrams = {}
+
+        for index,word in enumerate(strs):
+
+            word = list(word)
+            word.sort()
+            word = "".join(word)
+
+            if word in anagrams:
+                anagrams[word].append(strs[index])
+            else:
+                anagrams[word] = [strs[index]]
+
+
+
+        ans = []
+
+        for val in anagrams.values():
+            ans.append(val)
+            
+        
+        return ans
+
+    
+         
+        
