8dd0ca9b50873a68dde0b90c8b25617359ed07be
mogesTesema <moges2421@gmail.com>
2026-02-12 10:59:26 +0300

    major element count II  leetcode medium one problem solved

---
+++ leetcode/medium/major_element.py
@@ -0,0 +1,14 @@
+from collections import Counter
+from typing import List
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+
+        freq_nums = Counter(nums)
+        cut_off = len(nums)//3
+        ans = []
+
+        for key, val in freq_nums.items():
+            if val > cut_off:
+                ans.append(key)
+        return ans
+        
