4ee2daa72dea02afc6be98d0d1567b81893c0153
mogesTesema <moges2421@gmail.com>
2026-02-12 13:27:23 +0300

    top key frequent element leetcode medium one problem solved

---
+++ leetcode/easy/separate_digits.py
@@ -0,0 +1,12 @@
+from typing import List
+class Solution:
+    def separateDigits(self, nums: List[int]) -> List[int]:
+        ans = []
+
+        for num in nums:
+            num = [int(x) for x in str(num)]
+            ans.extend(num)
+        
+        return ans
+
+        
