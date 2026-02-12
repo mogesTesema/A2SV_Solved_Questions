7f1cf74846b9a8549353e475d61b0fd41c05289d
mogesTesema <moges2421@gmail.com>
2026-02-12 12:01:43 +0300

    three consecutive interger  leetcode medium one problem solved

---
+++ leetcode/medium/three_cunsecutive.py
@@ -0,0 +1,12 @@
+from typing import List
+class Solution:
+    def sumOfThree(self, num: int) -> List[int]:
+        smaller = num // 3
+        remainder = num % 3 
+        if remainder != 0:
+            return []
+        return [smaller -1, smaller,smaller +1]
+
+
+        
+        
