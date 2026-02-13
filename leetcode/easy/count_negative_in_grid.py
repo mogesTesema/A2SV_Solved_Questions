79d6988de2389498f87d0220e102f6d89ce3ed10
mogesTesema <moges2421@gmail.com>
2026-02-13 16:51:05 +0300

    leetcode problem solved

---
+++ leetcode/easy/count_negative_in_grid.py
@@ -0,0 +1,17 @@
+from typing import List
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+
+        rows,cols = len(grid),len(grid[0])
+        count = 0
+
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] < 0:
+                    count += 1 
+        
+        return count
+
+
+        
