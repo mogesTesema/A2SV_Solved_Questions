bdc6c7de35ba3720f8d9d28bf5874e144cc46fae
mogesTesema <moges2421@gmail.com>
2026-02-19 17:53:49 +0300

    problem solved

---
+++ leetcode/easy/transpose.py
@@ -0,0 +1,9 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        rows = len(matrix)
+        cols = len(matrix[0])
+        ans = [[0] * rows for _ in range(cols)]
+        for r in range(rows):
+            for c in range(cols):
+                ans[c][r] = matrix[r][c]
+        return ans
