7c36cd3c014d696f35366d60a70836d176e81a64
mogesTesema <moges2421@gmail.com>
2026-02-20 19:10:35 +0300

    one problem solved

---
+++ leetcode/easy/smaller_counter.py
@@ -0,0 +1,29 @@
+from typing import List
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+
+        index_nums = [[num,index] for index,num in enumerate(nums)]
+        index_nums.sort()
+        counter = 0
+        prev = index_nums[0][0]
+        ans = [0]*len(index_nums)
+
+
+        for i in range(len(index_nums)):
+            val = index_nums[i][0]
+            index = index_nums[i][1]
+
+            if val == prev:
+                ans[index] = counter
+                continue
+
+            ans[index] = i
+            prev = val
+            counter = i
+
+
+
+        return ans
+
+
+        
