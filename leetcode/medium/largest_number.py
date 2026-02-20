aaea4935b841d757620c69f7a041cf2f95f451d6
mogesTesema <moges2421@gmail.com>
2026-02-20 18:51:40 +0300

    one problem solved

---
+++ leetcode/medium/largest_number.py
@@ -0,0 +1,13 @@
+from typing import List
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        whole = []
+        for num in nums:
+            num = str(num)
+            whole.append(num)
+    
+        whole.sort(key=lambda x:x*12,reverse=True)
+        ans = "".join(whole)
+        return str(int(ans))
+
+        
