452bd292788208effcbbc0f57460f4b70e1c6545
mogesTesema <moges2421@gmail.com>
2026-03-11 19:35:05 +0300

    problem solved

---
+++ leetcode/medium/stack#temprature.py
@@ -0,0 +1,20 @@
+from typing import List
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+
+        ans = [0]*len(temperatures)
+
+        stack = []
+
+        for i, temp in enumerate(temperatures):
+            while stack and stack[-1][0] < temp:
+                top, index = stack.pop()
+                ans[index] = i -index
+            stack.append([temp,i])
+        return ans
+
+
+                
+                    
+
+        
