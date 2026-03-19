6a0af835a38f6d84bf42110b968815560757941b
mogesTesema <moges2421@gmail.com>
2026-03-19 20:10:09 +0300

    leetcode problem solved

---
+++ leetcode/medium/tree#even_grand.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def sumEvenGrandparent(self, root: Optional[TreeNode]) -> int:
+
+        ans = 0
+        def traverse(node):
+            nonlocal ans
+            if not node:
+                return
+
+            if node.val % 2 == 0:
+                if node.right:
+                    if node.right.right:
+                        ans += node.right.right.val
+                    if node.right.left:
+                        ans += node.right.left.val
+                if node.left:
+                    if node.left.right:
+                        ans += node.left.right.val
+                    if node.left.left:
+                        ans += node.left.left.val
+            traverse(node.left)
+            traverse(node.right)
+        
+        traverse(root)
+
+        return ans
+
+            
+        
