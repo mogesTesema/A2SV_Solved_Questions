fdaf0f588f59bcdf30573442c1e35ff1a9696dd8
Moges Tesema <moges.tesema@astu.edu.et>
2026-04-03 23:17:33 +0300

    Add solution for Path Sum III

---
+++ leetcode/0437-path-sum-iii/0437-path-sum-iii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        
+        def count_from(node, curr_sum):
+            if not node:
+                return 0
+            
+            curr_sum += node.val
+    
+            return (
+                (1 if curr_sum == targetSum else 0)
+                + count_from(node.left, curr_sum)
+                + count_from(node.right, curr_sum)
+            )
+        
+        if not root:
+            return 0
+        
+        return (
+            count_from(root, 0)
+            + self.pathSum(root.left, targetSum)
+            + self.pathSum(root.right, targetSum)
+        )
