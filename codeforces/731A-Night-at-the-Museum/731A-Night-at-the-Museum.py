3b613114f59c4494ed26c38e0cd3d17030d85b00
Moges Tesema <moges.tesema@astu.edu.et>
2026-04-07 05:24:09 +0300

    Add solution for Night at the Museum

--- codeforces/731A-Night-at-the-Museum/731A-Night-at-the-Museum.py
+++ codeforces/731A-Night-at-the-Museum/731A-Night-at-the-Museum.py
@@ -1,11 +1,11 @@
 import string
 
-exhibit = input()
+exhibits = input()
 lowercase = string.ascii_lowercase
 result = 0
 currentPosition = 0
 
-for char in exhibit:
+for char in exhibits:
     targetPosition = lowercase.index(char)
     distance = abs(targetPosition - currentPosition)
     result += min(distance, 26 - distance)  
