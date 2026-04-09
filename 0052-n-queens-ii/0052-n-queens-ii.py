83dd51ea6309c01eb0cda51647dbd7d4b5f811e9
Moges Tesema <moges.tesema@astu.edu.et>
2026-04-09 10:14:54 +0300

    Time: 7 ms (89.06%), Space: 19.2 MB (89.06%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -15,7 +15,7 @@ class Solution:
             
             for col in range(n):
                 curr_pos_dig = r + col
-                curr_neg_dig = r - col
+                curr_neg_dig = r - col 
 
                 if col in cols or curr_pos_dig in pos_dig or curr_neg_dig in neg_dig:
                     continue
