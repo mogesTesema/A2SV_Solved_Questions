08e621a810fd6289e090a649f156b7a54d33b7b2
Moges Tesema <moges.tesema@astu.edu.et>
2026-04-09 10:13:47 +0300

    Time: 108 ms (61.09%), Space: 45.9 MB (40.15%) - LeetHub

---
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxSumBST(self, root):
+        self.max_sum = 0
+
+        def dfs(node):
+            if not node:
+                return (True, 0, float('inf'), float('-inf'))
+            
+            left_is_bst, left_sum, left_min, left_max = dfs(node.left)
+            right_is_bst, right_sum, right_min, right_max = dfs(node.right)
+
+            # Check BST condition
+            if left_is_bst and right_is_bst and left_max < node.val < right_min:
+                curr_sum = left_sum + right_sum + node.val
+                self.max_sum = max(self.max_sum, curr_sum)
+                
+                return (
+                    True,
+                    curr_sum,
+                    min(left_min, node.val),
+                    max(right_max, node.val)
+                )
+            else:
+                return (False, 0, 0, 0)
+
+        dfs(root)
+        return self.max_sum
