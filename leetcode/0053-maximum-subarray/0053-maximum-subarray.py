a9a18e618b2d8b7cc8c41856cabae6ee7d52aa92
Moges Tesema <moges.tesema@astu.edu.et>
2026-04-11 11:57:42 +0300

    Add solution for Maximum Subarray

---
+++ leetcode/0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxSubArray(self, nums):
+        cur = nums[0]
+        best = nums[0]
+        
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            best = max(best, cur)
+        
+        return best
