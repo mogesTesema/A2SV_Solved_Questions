3a68232c0c17074f7408262531a6c99ad2f1997a
Moges Tesema <moges.tesema@astu.edu.et>
2026-04-17 19:05:07 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
@@ -4,25 +4,19 @@ class Solution:
             def backtrack(start, path):
 
                 if len(path) >= 2:
-                    flag = True
-                    for i in range(1, len(path)):
-                        if path[i] < path[i - 1]:
-                            flag = False
-                            break
-                    if flag: ans[tuple(path[:])] += 1
-                    else: return
+                    ans[tuple(path[:])] += 1
                 # path.append(nums[start])
                 for i in range(start,len(nums)):
-                    # if len(path):
-                    #     if nums[i] >= nums[i - 1]:
-                    #         path.append(nums[i])
-                    #         backtrack(i + 1, path)
-                    #         path.pop()
+                    if len(path):
+                        if nums[i] >= path[-1]:
+                            path.append(nums[i])
+                            backtrack(i + 1, path)
+                            path.pop()
                         
-                    # else:
-                    path.append(nums[i])
-                    backtrack(i + 1, path)
-                    path.pop()
+                    else:
+                        path.append(nums[i])
+                        backtrack(i + 1, path)
+                        path.pop()
                 
             backtrack(0,[])
             
