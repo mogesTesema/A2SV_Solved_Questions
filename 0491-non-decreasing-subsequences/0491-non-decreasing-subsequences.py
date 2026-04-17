a28f04b0931c07defa736de2a543c3baf731a936
Moges Tesema <moges.tesema@astu.edu.et>
2026-04-17 19:37:42 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
@@ -5,14 +5,12 @@ class Solution:
 
                 if len(path) >= 2:
                     ans[tuple(path[:])] += 1
-                # path.append(nums[start])
                 for i in range(start,len(nums)):
                     if len(path):
                         if nums[i] >= path[-1]:
                             path.append(nums[i])
                             backtrack(i + 1, path)
                             path.pop()
-                        
                     else:
                         path.append(nums[i])
                         backtrack(i + 1, path)
